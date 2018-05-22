e49b8169a6affdd3c070fc087a262de0cb862451
timsnyder <snydertimothym@gmail.com>
2018-05-22 16:56:46 -0400

    first commit

---
+++ Capstone4/JavaScript.js
@@ -0,0 +1,27 @@
+﻿
+
+var names = [];
+var prices = [];
+
+function addItem(name, price)
+{
+	names.push(name);
+	prices.push(price);
+
+	document.getElementById('cart').innerHTML += `<tr><td>${name}</td><td>${price}</td></tr>`;
+	getTotal();
+}
+
+function getTotal() {
+	var subTotal = 0;
+	for (var i = 0; i < prices.length; i++) {
+		subTotal += prices[i];
+
+	}
+
+	var total = subTotal * 1.06;
+	document.getElementById('total').innerHTML = `subtotal = ${subTotal.toFixed(2)}<br />total = ${total.toFixed(2)}`;
+
+
+
+}
